08d4f1f57bdf7ab4806d267c1feff985062fc7f6
Ahmed-Ibrahim-Salem <yussef916@gmail.com>
2018-04-23 11:36:50 +0200

    Moving the Enemy

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-
+    this.x += this.speed*dt;
 };
 
 // Draw the enemy on the screen, required method for game
